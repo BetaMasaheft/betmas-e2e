d09da4324677f5f931a2be60b08b2be3619475b2
duncdrum <d.paterson@me.com>
2023-01-08 21:43:11 +0100

    🐞 fix(e2e): bypass js error
    bypass leaflet.js error on load in beforeAll hook
    
    see #1

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,12 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    // see #1
+    if (err.message.includes('Map container not found')) {
+      return false
+    }
+  })
