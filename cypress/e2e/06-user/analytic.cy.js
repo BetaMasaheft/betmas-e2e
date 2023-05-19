456e10c1bbc845af3ada00e3be41e7b4d339f78a
duncdrum <d.paterson@me.com>
2023-05-19 18:11:21 +0200

    🐞 fix(analytic): up test coverage
    add more tests to subpage

--- cypress/e2e/06-user/analytic.cy.js
+++ cypress/e2e/06-user/analytic.cy.js
@@ -1,21 +1,65 @@
 describe('analytic page', () => {
   // see user 3
-    beforeEach(() => {
-        cy.visit('works/LIT1709Kebran/analytic')
-    })
+  beforeEach(() => {
+    cy.visit('works/LIT1709Kebran/analytic')
+  })
 
-    it('shows relationship table with data', () => {
-        cy.get('#BetMasRel')
-          .should('be.visible')
-        cy.get('.container.w3-half > .w3-panel')
-          .contains('relations found')
-        cy.get('tr')  
-          .should('have.length.of.at.least', 4)
-    })
+  it('shows relationship table with data', () => {
+    cy.get('#BetMasRel')
+      .should('be.visible')
+    cy.get('.container.w3-half > .w3-panel')
+      .contains('relations found')
+    cy.get('tr')
+      .should('have.length.of.at.least', 4)
+  })
 
-    // See User.md the graph is not working, aka empty
-    it('shows relationship graph area', () => {
-        cy.get('#BetMasRelView')
-          .should('be.visible')
+  // See User.md the graph is not working, aka empty
+  it('shows relationship graph area', () => {
+    cy.get('#BetMasRelView')
+      .should('be.visible')
+  })
+
+  describe('sidebar', () => {
+    it('shows 4 items in view', () => {
+      cy.get('#footer')
+        .scrollIntoView()
+      cy.get('#sidebar')
+        .should('be.visible')
+        .find('a')
+        .should('have.length', 4)
     })
+    it('contains a working Persons link', () => {
+      // (DP) see IndexPersons.cy.js
+      cy.get('[href^="/IndexPersons"]')
+        .then(function ($a) {
+          // extract the fully qualified href property
+          const href = $a.prop('href')
+
+          // make an http request for this resource
+          // outside of the browser
+          cy.request(href)
+            // drill into the response body
+            .its('body')
+            .should('include', '<h2>Index of person annotations</h2>')
+            .and('include', '</html>')
+        })
+    })
+    it('contains a working Works link', () => {
+      cy.get('[href^="/works"]')
+        .then(function ($a) {
+          // extract the fully qualified href property
+          const href = $a.prop('href')
+
+          // make an http request for this resource
+          // outside of the browser
+          cy.request(href)
+            // drill into the response body
+            .its('body')
+            // (DP) Might be to strict to include the title string
+            .should('include', '<h1 id="headtitle">Kǝbra nagaśt</h1>')
+            .and('include', '</html>')
+        })
+    })
+  })
+
 })
