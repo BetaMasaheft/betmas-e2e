b66cbd4444ab295180b912e883a24993b7cf76ca
Iwona Grabska-Gradzińska <grabska@gmail.com>
2023-08-03 23:42:27 +0200

    Creating test 18 in user stories, added ignored exceptions.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -27,3 +27,21 @@ Cypress.on('uncaught:exception', (err, runnable) => {
       return false
     }
   })
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    // see #1
+    if (err.message.includes('trimmed is undefined')) {
+      return false
+    }
+  })
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    // see #1
+    if (err.message.includes('data is null')) {
+      return false
+    }
+  })
