70043e4e84948935b0b49a26d57bfe66b9ef1252
helenasabel <helena.b.sabel@gmail.com>
2023-08-04 18:23:40 +0200

    test(lexicon): 03 user 19

---
+++ cypress/e2e/06-user/lexiconViewAttestations.cy.js
@@ -0,0 +1,14 @@
+describe('view attestations of a lemma', () => {
+    // see 03_user 19
+    it('click on page icon takes you to the relevant lexicon page', () => {
+        cy.visit('/Dillmann/lemma/La28f0d661a324ba5a2364e70e63ef317') 
+        cy.get('#loadattestations').click()
+        // check that the clicked button disappears
+        cy.get('#loadattestations').should('have.css', 'display', 'none')
+        // check presence of number of records after waiting for them to load
+        cy.wait(2500)
+        cy.get('#NumOfAtt').should('exist')
+        // check display of attestations
+        cy.get('#attestations').children().should('have.length.least', 1)
+    })
+})
