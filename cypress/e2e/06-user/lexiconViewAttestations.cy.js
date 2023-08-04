1205c28b74618fa681703e9124763d9fa0bef69e
helenasabel <helena.b.sabel@gmail.com>
2023-08-04 18:57:15 +0200

    style: change wording of descriptive messages

--- cypress/e2e/06-user/lexiconViewAttestations.cy.js
+++ cypress/e2e/06-user/lexiconViewAttestations.cy.js
@@ -1,6 +1,6 @@
 describe('view attestations of a lemma', () => {
     // see 03_user 19
-    it('click on page icon takes you to the relevant lexicon page', () => {
+    it('click on Load button loads the attestations', () => {
         cy.visit('/Dillmann/lemma/La28f0d661a324ba5a2364e70e63ef317') 
         cy.get('#loadattestations').click()
         // check that the clicked button disappears
