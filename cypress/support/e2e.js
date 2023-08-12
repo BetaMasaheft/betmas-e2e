e470ea45a3c68f1ab09aa2666420d7bbabf7066f
duncdrum <d.paterson@me.com>
2023-08-12 13:16:57 +0200

    🦄 refactor(dillmann): 03_user 18
    lemma search move code and annotate
    
    see #4

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -29,18 +29,18 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   })
 
   Cypress.on('uncaught:exception', (err, runnable) => {
-    // we expect a 3rd party library error with message 'list not defined'
+    // we expect a 3rd party library error with message 'trimmed is undefined'
     // and don't want to fail the test so we return false
-    // see #1
+    // see #4
     if (err.message.includes('trimmed is undefined')) {
       return false
     }
   })
 
   Cypress.on('uncaught:exception', (err, runnable) => {
-    // we expect a 3rd party library error with message 'list not defined'
+    // we expect a 3rd party library error with message 'data is null'
     // and don't want to fail the test so we return false
-    // see #1
+    // see #4
     if (err.message.includes('data is null')) {
       return false
     }
