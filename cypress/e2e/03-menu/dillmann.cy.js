2229b69f655cf2db5f89406c5bca107303628f4a
Iwona Grabska-Gradzińska <grabska@gmail.com>
2023-08-23 00:10:51 +0200

    fixes following the DP comments.

--- cypress/e2e/03-menu/dillmann.cy.js
+++ cypress/e2e/03-menu/dillmann.cy.js
@@ -7,9 +7,26 @@ describe('Dillman page', () => {
         // cy.on('uncaught:exception', (err, runnable) => {
         //     expect(err.message).to.include('Cannot read properties of undefined')
 
-        //     done()
+        //     // done()
         //     return false
         // })
+        // cy.on('uncaught:exception', (err, runnable) => {
+        //     // we expect a 3rd party library error with message 'trimmed is undefined'
+        //     // and don't want to fail the test so we return false
+        //     // see #4
+        //     if (err.message.includes('trimmed is undefined')) {
+        //       return false
+        //     }
+        //   })
+        
+        //   cy.on('uncaught:exception', (err, runnable) => {
+        //     // we expect a 3rd party library error with message 'data is null'
+        //     // and don't want to fail the test so we return false
+        //     // see #4
+        //     if (err.message.includes('data is null')) {
+        //       return false
+        //     }
+        //   })
         cy.visit('Dillmann/')
     })
 
@@ -25,9 +42,11 @@ describe('Dillman page', () => {
             cy.get('[name="q"]')
                 .type('ሀሰሰ')
 
+            
+            cy.get('[name="mode"]').should('have.value', 'none')
             // 3_user 18.3 default mode is "Normal, with homophones"
-            // @iwonagg your assertion isn't actualy testing this
-            cy.get('[name="mode"]').select('none')
+            // cy.get('[name="mode"]').contains('Normal, with homophones')
+            cy.get('[name="mode"]').find(':selected').contains('Normal, with homophones')
             cy.get('.fa-search').click()
             // 03_user 18.4
             cy.get('#results > .w3-row')
@@ -39,21 +58,17 @@ describe('Dillman page', () => {
                     const pagination_int = parseInt(value);
                     cy.get('#results > .w3-row').its('length').should('be.lte', pagination_int)
                 })
-            // @iwonagg we are effectively testing if the first hit for the searchterm remains stable
-            // I have no idea if that is the case. Generally, if we assert on the id of the hit, i would
-            // try to find a different means of selecting if from the list of results. Alternatively. make a weakrer assertion
-            // so that new documents won't change the test result. 
+ 
             cy.get('#results .w3-twothird > a').first().invoke('attr', 'href')
-                .should('be.eq', '?mode=none&q=ሀሰሰ&id=La28f0d661a324ba5a2364e70e63ef317')
+                // first link leads to page with correct mode and searched phrase and any id
+                .should('contain', '?mode=none&q=ሀሰሰ&id=')
                 .then(href => {
                     cy.request(href)
                         .its('status')
                         .should('eq', 200)
                 });
             // 03_user 18.5
-            // @iwonagg see above, using the first hit here is different, its just a way station any result will work
             cy.get('#results .w3-twothird').first().click()
-            // @iwonagg testcases (it) should always end in an assertion 
             // a record appears indicated by the newly visible h3 dillman section
             cy.get('.entry')
               .contains('Dillman')
@@ -65,17 +80,14 @@ describe('Dillman page', () => {
             // 3_user 18.3 
             // select mode of searching as Fuzzy Search
             cy.get('[name="mode"]').select('fuzzy')
-
+            
             // search for lemma ሀሰሰ
             cy.get('[name="q"]')
                 .type('ሀሰሰ')
                 .type('{enter}')
-            // @iwonagg the next 3 assertions just repeat those from the previous testcase,
-            // this is a good opportunity to switch things up a little and broaden coverage
             cy.get('#results > .w3-row')
                 .should('be.visible');
-            cy.get('h3')
-                .should('contain', 'You found "ሀሰሰ" in ')
+            cy.get('h3').find('#hit-count').contains(/\d+/)
 
             // the list of results has >= elements then pagination 
             cy.get('#results').invoke('attr', 'data-template-per-page')
@@ -84,12 +96,10 @@ describe('Dillman page', () => {
                     cy.get('#results > .w3-row').its('length').should('be.lte', pagination_int)
                 })
 
-            // @iwonagg s.a. we are now implicitely testing those search modes. by asserting that the
-            // first result has different ids. it would be better to make that an explicit assertion
-            // that doesn't rely on position in the result list
-            // first link leads to Lf7959209d07e4f6fa0e3f6fe00ff8428
+
+            // first link leads to page with correct mode and searched phrase and any id
             cy.get('#results .w3-twothird > a').first().invoke('attr', 'href')
-                .should('be.eq', '?mode=fuzzy&q=ሀሰሰ&id=Lf7959209d07e4f6fa0e3f6fe00ff8428')
+                .should('contain', '?mode=fuzzy&q=ሀሰሰ&id=')
                 .then(href => {
                     cy.request(href)
                         .its('status')
