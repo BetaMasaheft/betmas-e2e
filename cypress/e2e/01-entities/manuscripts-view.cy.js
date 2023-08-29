f9a88261d23e3a34f0d07711592dc0e13f1ae489
helenasabel <helena.b.sabel@gmail.com>
2023-08-29 11:39:41 +0200

    feat(entities): add tests for manuscript view

--- cypress/e2e/01-entities/manuscripts-view.cy.js
+++ cypress/e2e/01-entities/manuscripts-view.cy.js
@@ -1,9 +1,55 @@
-describe('Manuscripts viewer page', () => {
-    // see user 13, 16
+describe('Manuscripts: view images and content', () => {
+    // See user 13 and 16
+
     beforeEach(() => {
-        cy.visit('manuscripts/viewer')
+        cy.visit('/manuscripts/ESdz010')
     })
 
-    it.skip('', () => {
-    })
+    it('View manuscript images', () => {
+        // See 03_user 13
+        // In the top menu, click Images
+        cy.get('[href*="/manuscripts/ESdz010/viewer"]')
+        // HBS: I get a “too much recursion” error with the following code, 
+        // so I use 'removeAttr' instead
+        // .invoke('attr', 'href')
+        // .then(href => {
+        //   cy.request(href)
+        //     .its('body')
+        //     .should('include', '</html>')
+        //     .and('include', 'mirador-main-menu-bar')
+          //})
+        .invoke('removeAttr', 'target')
+        .click()
+        // The page opens with MIRADOR viewer and images visible
+        cy.get('.mirador-main-menu-bar')
+          .should('be.visible')
+          cy.get('li.highlight > .thumbnail-image')
+          .should('be.visible')
+        // To get to the main entry view, select Entry in the top menu, you will be redirected to https://betamasaheft.eu/manuscripts/ESap028/main  
+        cy.get('[href*="/manuscripts/ESdz010/main"]')
+        .invoke('attr', 'href')
+        .then(href => {
+          cy.request(href)
+            .its('body')
+            .should('include', '</html>')
+          })
+      })
+
+      it('View manuscript contents', () => {
+        // See 03_user 16
+        // Click on "contents" to expand view 
+        cy.get('button[resource="https://betamasaheft.eu/ESdz010/msitem/ms_i1"][onclick^="openAccordion"]')
+        .click()
+        // Click other eventual boxes to expand view 
+        cy.get('[onClick^=openAccordion][onClick*=itemms_i1-4]')  
+        // To get more information about a work contained, click on the underlined work title 
+        cy.get('[href="/LIT1544Gebrah"]')
+        .invoke('attr', 'href')
+        .then(href => {
+          cy.request(href)
+            .its('body')
+            .should('include', '</html>')
+          })
+      })
+
 })
