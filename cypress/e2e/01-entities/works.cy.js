a609b5b0b065e3c8cfd005fbe0b5124aebc52480
helenasabel <helena.b.sabel@gmail.com>
2023-09-22 17:07:32 +0200

    fix(test): better assertions in works page

--- cypress/e2e/01-entities/works.cy.js
+++ cypress/e2e/01-entities/works.cy.js
@@ -3,31 +3,35 @@ describe('A works page', () => {
 
     beforeEach(() => {
       //Go to the page of a work record
-        cy.visit('/works/LIT1631Homily')
+        cy.visit('/works/LIT1709Kebran')
     })
 
     it('View work text', () => {
         // see 03_user 14
       //In the top menu, click Text and the page opens
-      cy.get('[href*="/works/LIT1631Homily/text"]')
+      cy.get('[href*="/works/LIT1709Kebran/text"]')
         .invoke('attr', 'href')
         .then(href => {
           cy.request(href)
             .its('body')
-            .should('include', '</html>')
+            .should('include', '<div id="transcription">')
           })
     })
 
     it('See the witnesses of a work', () => {
       // see 03_user 15
-//See the red box on the right "This unit, or parts of it, is contained in 13 manuscript records 13 times"
-//For more details on the witness, click on the underlined shelfmark in the list image
-cy.get('[href*="/manuscripts/ESdz010"]')
+      //See the red box on the right "This unit, or parts of it, is contained in 13 manuscript records 13 times"
+      cy.get('#computedWitnesses .openInDialog').invoke('text').should(text => {
+        expect(text).to.match(/This unit, or parts of it, is contained in [0-9]+ manuscript records [0-9]+ times/i)
+      })
+      //For more details on the witness, click on the underlined shelfmark in the list image
+      cy.get('[href*="/manuscripts/BLorient818"]')
       .invoke('attr', 'href')
       .then(href => {
         cy.request(href)
           .its('body')
           .should('include', '</html>')
+          .and('include', 'BL Oriental 818')
         })
   })
 })
