386eb8c888b879ff3f9b2bd9785e776cc04f2731
Iwona Grabska-Gradzińska <grabska@gmail.com>
2023-10-03 00:46:54 +0200

    02_03  and 02_04  in progress

--- cypress/e2e/08-contributor/dillmann.cy.js
+++ cypress/e2e/08-contributor/dillmann.cy.js
@@ -1,4 +1,4 @@
-const username = "JinntecLexicon" // Cypress.env('username')
+const username = "JinntecLexicon" 
 const password = Cypress.env('password_lex')
 
 describe('Dillman page', () => {
@@ -19,8 +19,8 @@ describe('Dillman page', () => {
 
     
 
-    describe('searchSee activity', () => {
-        // see 02_03
+    describe('See activity', () => {
+        // see 02_03    
         it('logs in using env variables', () => {
             expect(username, 'username was set').to.be.a('string').and.not.be.empty
             expect(password, 'password was set').to.be.a('string').and.not.be.empty        
@@ -29,17 +29,79 @@ describe('Dillman page', () => {
           it('See activity', () => {
             // Click on "Hi, USERNAME!"
             cy.get('[title=about] > a')
+            //See the page documenting activities (The last 50 pages you visited)
             .invoke('attr', 'href')
             .then(href => {
                 cy.request(href)
                     .its('body')
-                    .should('include','Your made') //
-                    .should('include',username)
+                    .should('include', 'The last 50 pages you visited') 
+
             })
-            
+            //See the page documenting activities (Your made XXX changes in these files after the last conversion of the data from the original txt)
+            cy.get('[title=about] > a')
+            .invoke('attr', 'href')
+            .then(href => {
+                cy.visit(href)
+                .get('.w3-lightygreen > h3').invoke('text').then((text) => {
+                    // Use a regular expression to extract the numbers
+                    const regex = /Your made (\d+) changes in these files after the last conversion of the data from the original txt \((\d+)\.(\d+)\.(\d+)\)/;
+                    const matches = text.match(regex);
+                     
+                    // matches will contain an array of matched values
+                    if (matches) {
+                        const how_many_text = matches[1]; // Group 1 contains the captured number
+                        const how_many = parseInt(how_many_text, 10);
+                        expect(how_many).to.be.greaterThan(0);
+
+                        const day_text = matches[2]; // Group 1 contains the captured number
+                        const day = parseInt(day_text, 10);
+                        cy.wrap(day).should('be.lte', 31)
+                        .and('be.gte', 1)
+
+                        const month_text = matches[3]; // Group 1 contains the captured number
+                        const month = parseInt(month_text, 10);
+                        cy.wrap(month).should('be.lte', 12)
+                        .and('be.gte', 1)
+
+                        const currentDate = new Date();
+                        const currentYear = currentDate.getFullYear();
+
+                        const year_text = matches[4]; // Group 1 contains the captured number
+                        const year = parseInt(year_text, 10);
+                        cy.wrap(year).should('be.lte', currentYear)
+                        .and('be.gte', 2000)
+                    }
+                })
+            })
            });
+    })
 
+    describe('Create new entry', () => {
+        // see 02_04    
+        it('logs in using env variables', () => {
+            expect(username, 'username was set').to.be.a('string').and.not.be.empty
+            expect(password, 'password was set').to.be.a('string').and.not.be.empty        
+          });
 
-        
+          it('Create new entry existing', () => {
+            // Click "New Entry" button
+            cy.contains("New Entry").click()
+            cy.get('#form')
+            .type('አልማዲ') 
+            cy.get('.alert-warning').invoke('text')
+            .should('include','Be carefull, this lemma is already there! See: ')
+            // .should('include','this is a new lemma')
+            
+        });
+
+        it('Create new entry not existing', () => {
+            // Click "New Entry" button
+            cy.contains("New Entry").click()
+            cy.get('#form')
+            .type('dupa') 
+            cy.get('.alert-success').invoke('text')
+            .should('include','this is a new lemma!')
+            
+        });
     })
 })
