ade48ea1a2facb4231f94f8f26ea0b257d79a019
duncdrum <d.paterson@me.com>
2026-07-24 14:01:31 +0200

    test(support): remove Map container not found bypass
    The bypass was masking a real bug in BetMasWeb's geojsonmap.js, not a
    3rd-party-library quirk to work around. Now that the source is fixed,
    removing the bypass so a regression would fail loudly again.
    
    Refs #1
    Fixes BetaMasaheft/BetMasWeb#43

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -25,16 +25,6 @@ registerCypressGrep()
 // require('./commands')
 
 Cypress.on('uncaught:exception', (err, _runnable) => {
-
-  // we expect a 3rd party library error with message 'list not defined'
-  // and don't want to fail the test so we return false
-  // see #1
-  if (err.message.includes('Map container not found')) {
-    return false
-  }
-})
-
-Cypress.on('uncaught:exception', (err, _runnable) => {
   // we expect a error with message 'form is not defined'
   // on the advanced search submit button
   // see #14
